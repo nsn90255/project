6ae6b89b4158732b5f0d51f737b75b6d44ce82e0
nsn90255 <nsn90255@gmail.com>
2024-12-07 21:59:40 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+javac Project_main.java
+jar cfm Project_main.jar MANIFEST.MF Project_main.class
+cp Project_main.jar /usr/local/bin/project.jar
+chmod 744 /usr/local/bin/project.jar
+echo "#!/bin/sh" > /usr/local/bin/project
+echo "java -jar /usr/local/bin/project.jar "$@"" >> /usr/local/bin/project
