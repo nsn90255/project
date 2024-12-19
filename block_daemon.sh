b023dbfc5c9eb46372368e037124085e363d3418
nsn90255 <nsn90255@gmail.com>
2024-12-19 16:37:56 +0100

    Loop forever

--- block_daemon.sh
+++ block_daemon.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-day=$(date +%u)
-hour=$(date +%H)
-minute=$(date +%M)
-if [ "$( grep $day /etc/blocklist.conf | awk -F ' : ' '{print $2}')" = "all" ]
-then
-	echo 'this is the part where you block'
-else 
-	echo 'not blocking'
-fi 
+while :
+do
+	day=$(date +%u)
+	hour=$(date +%H)
+	minute=$(date +%M)
+	if [ "$( grep $day /etc/blocklist.conf | awk -F ' : ' '{print $2}')" = "all" ]
+		then
+			echo 'this is the part where you block'
+		else 
+			echo 'not blocking'
+	fi 
+	sleep 60
+done
