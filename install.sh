7e7fa34a7bc23dfb213222922e5264965118f98e
nsn90255 <nsn90255@gmail.com>
2025-01-08 19:34:42 +0100

    Move init checking up

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ if [ "$(basename $(pwd))" != project ]; then
 	echo "Run in main project directory"
 	exit 1
 fi
+if [ -d /lib/systemd ]; then
+	cp systemd/project.service /etc/systemd/system/project.service
+	cp block_daemon.sh /usr/local/bin/block_daemon.sh
+else
+	echo "This program only supports systemd for now."
+fi
 if [ -d build ]; then
 	rm -r build
 fi
@@ -16,10 +22,4 @@ chmod 744 /usr/local/bin/project.jar
 echo '#!/bin/sh' > /usr/local/bin/project
 echo 'java -jar /usr/local/bin/project.jar "$@"' >> /usr/local/bin/project
 cd ../
-cp block_daemon.sh /usr/local/bin/block_daemon.sh
-if [ -d /lib/systemd ]; then
-	cp systemd/project.service /etc/systemd/system/project.service
-else
-	echo "This program only supports systemd for now."
-fi
 echo "tada!!"
