9425020a8644fecbfabe13d2df99a9abcad2cb8f
nsn90255 <nsn90255@gmail.com>
2025-01-08 18:58:21 +0100

    Change dir

--- install.sh
+++ install.sh
@@ -10,4 +10,9 @@ cp Project_main.jar /usr/local/bin/project.jar
 chmod 744 /usr/local/bin/project.jar
 echo '#!/bin/sh' > /usr/local/bin/project
 echo 'java -jar /usr/local/bin/project.jar "$@"' >> /usr/local/bin/project
+cd ../
+cp block_daemon.sh /usr/local/bin/block_daemon.sh
+if [ -d /lib/systemd ]; then
+	cp systemd/project.service /etc/systemd/system/project.service
+fi
 echo "tada!!"
