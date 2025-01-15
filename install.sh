1c48336bea2145b26998f1926ee7b7526e4ef198
nsn90255 <nsn90255@gmail.com>
2025-01-15 20:33:44 +0100

    Fix typo

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ if ! [ -d /usr/local/man/man8 ]; then
 	mkdir /usr/local/man/man8
 fi
 cp man_pages/project.8 /usr/local/man/man8/project.8
+if ! [ -d /usr/local/man/man5 ]; then
+	mkdir /usr/local/man/man5
+fi
+cp man_pages/blocklist.conf.5 /usr/local/man/man5/blocklist.conf.5
 if [ -d build ]; then
 	rm -r build
 fi
