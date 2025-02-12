ba5ff172e089bcc65737889e418ead3cc2eac9c3
nsn90255 <nsn90255@gmail.com>
2025-02-12 20:49:55 +0100

    Fix oversight

--- block_daemon.sh
+++ block_daemon.sh
@@ -17,17 +17,23 @@ do
 	third_time=$(echo "$extract_after_day" | awk -F '-' '{print $3}')
 	fourth_time=$(echo "$extract_after_day" | awk -F '-' '{print $4}')
 	# make check for empty log at some point
-	# check log to see if it should ignore (works for the same day for now)
+	# check log to see if it should ignore (assume it's the same day)
 	if [ "$(echo "$hour - $lastBlockHour" | bc)" = 0 ];then
 		howManyMinutes="$(echo "$minute - $lastBlockMinute" | bc)"
-		howManyMinutesInSeconds="$(echo "$howManyMinutes*60" | bc)"
+		howManyMinutes2="$(echo "60 - $howManyMinutes" | bc)"
+		howManyMinutesInSeconds="$(echo "$howManyMinutes2*60" | bc)"
 		sleepFor="$(echo "3600 - $howManyMinutesInSeconds" | bc)"
-		echo minute $minute
-		echo lastBlockMinute $lastBlockMinute
 		echo sleepFor $sleepFor
+	elif [ "$(echo "$hour - $lastBlockHour" | bc)" = 1 ];then
+		minutesToNextHour="$(echo "60 - $lastBlockMinute" | bc)"
+		minutesDifference="$(echo "$minutesToNextHour + $minute" | bc)"
+		echo minutesDifference $minutesDifference 
 	else
 		echo not so sure
 	fi
+	echo minute $minute
+	echo lastBlockMinute $lastBlockMinute
+	echo sleepFor $sleepFor
 	# check the config at /etc/blocklist.conf to see if it's okay to block
 	if [ "$extract_after_day" = "all" ];then
 		echo first if
